77bec7bd932092ab248c50fe300ff3df5a92ff5c
racostyle <cekcar@gmail.com>
2024-11-14 18:17:57 +0100

    ApiCore: small host update

--- ApiHost/Program.cs
+++ ApiHost/Program.cs
@@ -1,4 +1,6 @@
 
+using System.Text.Json;
+
 namespace ApiHost
 {
     public class Program
@@ -6,11 +8,23 @@ namespace ApiHost
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
 
+            //builder.WebHost.ConfigureKestrel(serverOptions =>
+            //{
+            //    serverOptions.ListenAnyIP(5337); // HTTP on port 5337
+            //    serverOptions.ListenAnyIP(8337, listenOptions =>
+            //    {
+            //        listenOptions.UseHttps(); // HTTPS on port 8337
+            //    });
+            //});
+
+            builder.Services.AddSingleton<Settings>(); //single instance injected each time and persist for aplication lifetime
+            //builder.Services.AddScoped<Settings>(); //single  
+            //builder.Services.AddTransient<Settings>();
+
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -24,13 +38,32 @@ namespace ApiHost
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
+
+        }
+    }
+
+    public class Settings
+    {
+        private const string SQL_SERVER_NAME = "SqlServer";
+        private const string CONFIG_NAME = "appsettings.Secrets.json";
+
+        private Dictionary<string, string> _config;
+
+        public Settings()
+        {
+            LoadSettings();
+        }
+
+        internal void LoadSettings()
+        {
+            var json = File.ReadAllText(CONFIG_NAME);
+            if (!string.IsNullOrEmpty(json))
+            {
+                _config = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+            }
         }
     }
 }
